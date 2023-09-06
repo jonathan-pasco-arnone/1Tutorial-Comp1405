2c4ec59d6bd18eab031dd7c6f81fb69101aa8729
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-09-06 01:06:23 +0000

    linted

--- finding_largest_number.py
+++ finding_largest_number.py
@@ -6,10 +6,9 @@
 
 import random
 
-# Finds the x biggest number in an array
 def x_biggest_number(array, x_highest):
+    """Finds the x biggest number in an array"""
     sorted_array = []
-    
     # Finds highest numbers from largest to smallest
     increment = 0
     while increment < len(array):
@@ -24,29 +23,31 @@ def x_biggest_number(array, x_highest):
         increment += 1
 
     print(sorted_array)
-    return(sorted_array[x_highest - 1])
+    return sorted_array[x_highest - 1]
 
 def main():
+    """Main function"""
 
     # Generates random array of 10 numbers
-    list_of_numbers = [random.randint(0,100), random.randint(0,100),
-            random.randint(0,100), random.randint(0,100),
-            random.randint(0,100), random.randint(0,100),
-            random.randint(0,100), random.randint(0,100),
-            random.randint(0,100), random.randint(0,100)]
+    list_of_numbers = [random.randint(0, 100), random.randint(0, 100),
+                       random.randint(0, 100), random.randint(0, 100),
+                       random.randint(0, 100), random.randint(0, 100),
+                       random.randint(0, 100), random.randint(0, 100),
+                       random.randint(0, 100), random.randint(0, 100)]
 
     print("List of 10 numbers", list_of_numbers)
     # Makes sure the input is valid
     try:
         x_largest = int(input("which largest number would you like to guess"
-                + " (1st, 2nd, 3rd, etc.)? "))
+                              + " (1st, 2nd, 3rd, etc.)? "))
         if x_largest < 1 or x_largest > 10:
             print("please input a real number from 1-10")
         else:
             print(x_biggest_number(list_of_numbers, x_largest))
-            
+
     except:
         print("Pleas input an actual number")
 
+
 if __name__ == "__main__":
-        main()
+    main()
